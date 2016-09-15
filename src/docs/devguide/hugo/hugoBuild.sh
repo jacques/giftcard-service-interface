6d9aac171ec61cde1ec4015b02c80a6d9ed977fa
TimElectrum <timwhelan@electrum.co.za>
2016-09-15 16:23:35 +0200

    fool

--- src/docs/devguide/hugo/hugoBuild.sh
+++ src/docs/devguide/hugo/hugoBuild.sh
@@ -35,8 +35,8 @@ echo ''
 echo ''
 echo '  + Running hugo build'
 echo ''
-docker run --name "hugo" -v ${BASE_DIR}/target/devguide/hugo:/src -v ${BASE_DIR}/target/devguide/site:/output -e "HUGO_THEME=hugo-material-docs" -e "HUGO_BASEURL=https://electrumpayments.github.io/giftcard-service-interface-docs/" jojomi/hugo
-#docker run --name "hugo" -v ${BASE_DIR}/target/devguide/hugo:/src -v ${BASE_DIR}/target/devguide/site:/output -e "HUGO_THEME=hugo-material-docs" -e "HUGO_BASEURL=/" jojomi/hugo
+#docker run --name "hugo" -v ${BASE_DIR}/target/devguide/hugo:/src -v ${BASE_DIR}/target/devguide/site:/output -e "HUGO_THEME=hugo-material-docs" -e "HUGO_BASEURL=https://electrumpayments.github.io/giftcard-service-interface-docs/" jojomi/hugo
+docker run --name "hugo" -v ${BASE_DIR}/target/devguide/hugo:/src -v ${BASE_DIR}/target/devguide/site:/output -e "HUGO_THEME=hugo-material-docs" -e "HUGO_BASEURL=/" jojomi/hugo
 
 docker stop hugo &> /dev/null
 docker rm hugo &> /dev/null
